22886b77d56890c0b7d89a8599b1b31812c60678
Mirko Kukoleca <mirko@invenit.io>
2019-08-23 11:27:20 +0200

    Kalkulator2 updates

--- 23.08.2019/kalkulator2.php
+++ 23.08.2019/kalkulator2.php
@@ -2,12 +2,12 @@
 <h1>Jednostavan kalkulator sa odabirom operacije</h1>
 
 <p>
-    U ovom primjeru se koriste GET varijable za prosljedjivanje parametara / podataka na server te se tamo obradjuju i server vraca rezultat. Takodje se salje i operacija kojom zelimo da tretiramo brojeve.
+    U ovom primjeru se koriste POST varijable za prosljedjivanje parametara / podataka na server te se tamo obradjuju i server vraca rezultat. Takodje se salje i operacija kojom zelimo da tretiramo brojeve.
 </p>
 
-<form action="kalkulator2.php" method="GET">
-     Num1: <input type="number" name="num1" value="<?= $_GET['num1'] ?>">
-     Num2: <input type="number" name="num2" value="<?= $_GET['num2'] ?>">
+<form action="kalkulator2.php" method="POST">
+     Num1: <input type="number" name="num1" value="<?= $_POST['num1'] ?>">
+     Num2: <input type="number" name="num2" value="<?= $_POST['num2'] ?>">
      <input type="submit" value="+" name="oper">
      <input type="submit" value="-" name="oper">
      <input type="submit" value="%" name="oper">
@@ -15,14 +15,33 @@
 </form>
 
 <?php
-    $num1 = $_GET['num1'];
-    $num2 = $_GET['num2'];
-    $oper = $_GET['oper'];
+    $num1 = $_POST['num1'];
+    $num2 = $_POST['num2'];
+    $oper = $_POST['oper'];
     echo 'Rezultat: ' . number_format(calculate($num1, $num2, $oper), 2);
 
 
     function calculate($num1, $num2, $oper) {
         switch ($oper) {
+            case '+':
+                return $num1 + $num2;
+            case '-':
+                return $num1 - $num2;
+            case '%':
+                return $num1 / $num2;
+            case '*':
+                return $num1 * $num2;
+            default:
+                return 0;
+        }
+    }
+?>
+<p>
+    Funkcija za obradu brojeva:
+</p>
+<pre>
+function calculate($num1, $num2, $oper) {
+    switch ($oper) {
         case '+':
             return $num1 + $num2;
         case '-':
@@ -32,7 +51,7 @@
         case '*':
             return $num1 * $num2;
         default:
-            return 'ERROR';
-        }
+            return 0;
     }
-?>
+}
+</pre>
